77f217836b822b2782d16c90d68414f2ce547acc
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-08 17:37:40 -0500

    - added 1 image for display purposes

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-let child_process = require("child_process")
+// let child_process = require("child_process")
 let { app, BrowserWindow } = require("electron")
 
-
-let runCommand = (command) => {
-  return child_process.execSync(command).toString().trim()
-}
-
-// communicate with the front end
-let toQueryString = (obj) => {
-  let q = []
-  for (let key in obj) {
-    q.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
-  }
-  return q.join("&")
-}
-
-
-// retrieve operating information
-let sysInfo = {
-  os: runCommand("uname -s"),
-  cpu: runCommand("uname -m"),
-  hostname: runCommand("hostname -s"),
-  ip: runCommand("ipConfig getifaddr en0"),
-}
+let html = `
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Welcome to the Internet!</h1>
+  </body>
+</html>
+`
+// let runCommand = (command) => {
+//   return child_process.execSync(command).toString().trim()
+// }
+// 
+// // communicate with the front end
+// let toQueryString = (obj) => {
+//   let q = []
+//   for (let key in obj) {
+//     q.push(`${encodeURIComponent(key)}=${encodeURIComponent(obj[key])}`)
+//   }
+//   return q.join("&")
+// }
+// 
+// 
+// // retrieve operating information
+// let sysInfo = {
+//   os: runCommand("uname -s"),
+//   cpu: runCommand("uname -m"),
+//   hostname: runCommand("hostname -s"),
+//   ip: runCommand("ipConfig getifaddr en0"),
+// }
 
 function createWindow() {
   let win = new BrowserWindow({})
   win.maximize()
-  win.loadFile(`file:${__dirname}/index.html?${toQueryString(sysInfo)}`) // finds the index.html in any directory
+  win.loadFile(`data:text/html;charset=utf-8,${encodeURI(html)}`) // reads the html variable from above
 }
 
 app.on("ready", createWindow)
