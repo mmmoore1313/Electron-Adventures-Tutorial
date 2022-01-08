80798b2e63f6f2e7ac459d5fc1b1fd9a1d79896f
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-08 17:38:03 -0500

    NA

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // let child_process = require("child_process")
+let fs = require("fs")
+let path = require("path")
 let { app, BrowserWindow } = require("electron")
+
+let imageDir
+let argv = process.argv
+
+if (argv.length >= 3) {
+  imageDir = argv[2]
+} else {
+  imageDir = `${__dirname}/images`
+}
+
+let findImages = (dir) => {
+  let files = fs.readeirSync(dir)
+  files.sort()
+  return files
+    .filter(x => /\.(png|jpg|jpeg|gif)/i.test(x))
+    .map(x => path.join(dir, x))
+}
 
 let html = `
 <!DOCTYPE html>
 <html>
   <body>
     <h1>Welcome to the Internet!</h1>
+    <h2>Image Gallery - ${imageDir}</h2>
   </body>
 </html>
 `
@@ -34,7 +54,7 @@ let html = `
 function createWindow() {
   let win = new BrowserWindow({})
   win.maximize()
-  win.loadFile(`data:text/html;charset=utf-8,${encodeURI(html)}`) // reads the html variable from above
+  win.loadURL(`data:text/html;charset=utf-8,${encodeURI(html)}`) // reads the html variable from above
 }
 
 app.on("ready", createWindow)
