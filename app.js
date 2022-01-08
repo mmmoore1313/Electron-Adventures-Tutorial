0c924f5ee6f4779a44c09ce3d4b169f1441027b5
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-08 16:03:58 -0500

    - data = new URLSearchParams(document.location.search) -- creates the data variable to find the index.html and display the systeminfo - info = document.querySelector(#info) -- creates the info variable and assigns the html id #info - function (const [key, value] of data) -- takes the data object from the sysInfo and places it into the <div id=info>

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let counter = 0
 let div = document.querySelector('#counter')
 let button = document.querySelector('button')
+let data = new URLSearchParams(document.location.search)
+let info = document.querySelector('#info')
 
 let updateCounter = () => {
   div.innerHTML = `You clicked the button ${counter}`
@@ -10,5 +12,10 @@ button.addEventListener('click', () => {
   counter++
   updateCounter()
 })
-
 updateCounter()
+
+for (const [key, value] of data) {
+  let p = document.createElement("p")
+  p.append(`${key} = ${value}`)
+  info.append(p)
+}
