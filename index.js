8b1a30cd6b2bc27525117d56fc3a5bb572cc9215
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-08 15:58:35 -0500

    changed back to my slightly annotated version

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let child_process = require("child_process")
 let { app, BrowserWindow } = require("electron")
 
+
 let runCommand = (command) => {
   return child_process.execSync(command).toString().trim()
 }
 
+// communicate with the front end
 let toQueryString = (obj) => {
   let q = []
   for (let key in obj) {
@@ -13,17 +15,19 @@ let toQueryString = (obj) => {
   return q.join("&")
 }
 
+
+// retrieve operating information
 let sysInfo = {
   os: runCommand("uname -s"),
   cpu: runCommand("uname -m"),
   hostname: runCommand("hostname -s"),
-  ip: runCommand("ipconfig getifaddr en0"),
+  ip: runCommand("ipConfig getifaddr en0"),
 }
 
 function createWindow() {
   let win = new BrowserWindow({})
   win.maximize()
-  win.loadURL(`file:${__dirname}/index.html?${toQueryString(sysInfo)}`)
+  win.loadFile(`file:${__dirname}/index.html?${toQueryString(sysInfo)}`) // finds the index.html in any directory
 }
 
 app.on("ready", createWindow)
