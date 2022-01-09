d13b7fdf4177a4a7f6d4f1c400124782e1e972c8
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-09 11:59:40 -0500

    commented out the last episode

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // episode 5: Display Free Disk Space
-
-let child_process = require("child_process")
-let { app, BrowserWindow } = require("electron")
-
-let runCommand = (command) => {
-  return child_process.execSync(command).toString().trim()
-}
-
-function df() {
-  let output = runCommand("df -kP")
-  let rows = output
-    .split(/[\r\n]+/)
-    .slice(1)
-  return rows.map(row => (
-    row
-      .replace(/\s+(\d+)/g, '\t$1')
-      .replace(/\s+\//g, '\t/')
-      .split(/\t/)
-  ))
-}
-
-function createWindow() {
-  let payload = JSON.stringify(df())
-  let win = new BrowserWindow({})
-  win.maximize()
-  win.loadFile('index.html') // loads the html in the index.html
-  
-  win.webContents.on('did-finish-load', function () {
-    win.webContents.executeJavaScript(`displayFreeDiskSpace(${payload})`)
-  })
-}
-
-
-app.on("ready", createWindow)
-
-app.on("window-all-closed", () => {
-  app.quit()
-})
+// 
+// let child_process = require("child_process")
+// let { app, BrowserWindow } = require("electron")
+// 
+// let runCommand = (command) => {
+//   return child_process.execSync(command).toString().trim()
+// }
+// 
+// function df() {
+//   let output = runCommand("df -kP")
+//   let rows = output
+//     .split(/[\r\n]+/)
+//     .slice(1)
+//   return rows.map(row => (
+//     row
+//       .replace(/\s+(\d+)/g, '\t$1')
+//       .replace(/\s+\//g, '\t/')
+//       .split(/\t/)
+//   ))
+// }
+// 
+// function createWindow() {
+//   let payload = JSON.stringify(df())
+//   let win = new BrowserWindow({})
+//   win.maximize()
+//   win.loadFile('index.html') // loads the html in the index.html
+// 
+//   win.webContents.on('did-finish-load', function () {
+//     win.webContents.executeJavaScript(`displayFreeDiskSpace(${payload})`)
+//   })
+// }
+// 
+// 
+// app.on("ready", createWindow)
+// 
+// app.on("window-all-closed", () => {
+//   app.quit()
+// })
 
 
 // episode 4: Image Gallery with Dynamic HTML
