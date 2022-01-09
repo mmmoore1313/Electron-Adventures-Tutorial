0bad3a44865afff895b5b61c101f03cf423e4ee5
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-08 19:15:47 -0500

    - commented out the episode3 code

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-let counter = 0
-let div = document.querySelector('#counter')
-let button = document.querySelector('button')
-let data = new URLSearchParams(document.location.search)
-let info = document.querySelector('#info')
+// episode3: What Can BAckend Code Even Do?
+// let data = new URLSearchParams(document.location.search)
+// let info = document.querySelector('#info')
+// 
+// for (const [key, value] of data) {
+//   let p = document.createElement("p")
+//   p.append(`${key} = ${value}`)
+//   info.append(p)
+// }
 
-let updateCounter = () => {
-  div.innerHTML = `You clicked the button ${counter}`
-}
-
-button.addEventListener('click', () => {
-  counter++
-  updateCounter()
-})
-updateCounter()
-
-for (const [key, value] of data) {
-  let p = document.createElement("p")
-  p.append(`${key} = ${value}`)
-  info.append(p)
-}
+// episode 2: Frontend Code and BAckend code
+// let counter = 0
+// let div = document.querySelector('#counter')
+// let button = document.querySelector('button')
+// 
+// let updateCounter = () => {
+//   div.innerHTML = `You clicked the button ${counter}`
+// }
+// 
+// button.addEventListener('click', () => {
+//   counter++
+//   updateCounter()
+// })
+// 
+// updateCounter()
