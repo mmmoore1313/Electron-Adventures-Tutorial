be2bd4d98813059acf53f73616c5971d9a06af9a
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-09 12:00:25 -0500

    added episode 2 to the bottom

--- index.js
+++ index.js
@@ -134,3 +134,19 @@
 //   app.quit()
 // })
 
+// episode 2
+// 
+// let { app, BrowserWindow } = require("electron")
+// 
+// function createWindow() {
+//   let win = new BrowserWindow({})
+//   win.maximize()
+//   win.loadFile("index.html")
+// }
+// 
+// app.on("ready", createWindow)
+// 
+// app.on("window-all-closed", () => {
+//   app.quit()
+// })
+
