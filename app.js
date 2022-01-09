66323c9db69ac23334e9a757e461481d9168b43d
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-09 11:49:17 -0500

    - defined the displayFreeDiskSpace function

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// episode 5: Display Free Disk Space
+function displayFreeDiskSpace(data) {
+  let infor = document.querySelector('#info')
+  for (let row of data) {
+    let total = parseInt(row[1])
+    let used = parseInt(row[2])
+    let path = row[5]
+    let percent
+    if (total !== 0) {
+      percent = Math.round(100 * used / total)
+    } else {
+      percent = 0
+    }
+    let dic = document.createElement("div")
+    div.append(`${path}: ${percent}% used (${used}/${total})`)
+    info.append(div)
+  }
+}
+
 // episode 4: Image Gallery with Dynamic HTML
 // Didn't do very much with the app.js here; all of the programming is found in the index.js
 
