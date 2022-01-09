92ce4e0bb23890d354b0a2dc27fca22edee93f9f
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-08 19:19:49 -0500

    - added the episode4,3,2 commentary to the bottom

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-let counter = 0
-let div = document.querySelector('#counter')
-let button = document.querySelector('button')
-let data = new URLSearchParams(document.location.search)
-let info = document.querySelector('#info')
+// episode 4: Image Gallery with Dynamic HTML
+// Didn't do very much with the app.js here; all of the programming is found in the index.js
 
-let updateCounter = () => {
-  div.innerHTML = `You clicked the button ${counter}`
-}
+// episode3: What Can BAckend Code Even Do?
+// let data = new URLSearchParams(document.location.search)
+// let info = document.querySelector('#info')
+// 
+// for (const [key, value] of data) {
+//   let p = document.createElement("p")
+//   p.append(`${key} = ${value}`)
+//   info.append(p)
+// }
 
-button.addEventListener('click', () => {
-  counter++
-  updateCounter()
-})
-updateCounter()
-
-for (const [key, value] of data) {
-  let p = document.createElement("p")
-  p.append(`${key} = ${value}`)
-  info.append(p)
-}
+// episode 2: Frontend Code and BAckend code
+// let counter = 0
+// let div = document.querySelector('#counter')
+// let button = document.querySelector('button')
+// 
+// let updateCounter = () => {
+//   div.innerHTML = `You clicked the button ${counter}`
+// }
+// 
+// button.addEventListener('click', () => {
+//   counter++
+//   updateCounter()
+// })
+// 
+// updateCounter()
