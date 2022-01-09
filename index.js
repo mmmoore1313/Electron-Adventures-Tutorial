98666e735b06dfc5d7237919183d779684ccbaf0
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-09 12:04:52 -0500

    - copied the episode 6 backend code as it is untouched for this exercise

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+// episode 6: Use D3 to Visualize Free Disk Space
+let child_process = require("child_process")
 let { app, BrowserWindow } = require("electron")
 
+let runCommand = (command) => {
+  return child_process.execSync(command).toString().trim()
+}
+
+function df() {
+  let output = runCommand("df -kP")
+  let rows = output
+    .split(/[\r\n]+/)
+    .slice(1)
+  return rows.map(row => (
+    row
+      .replace(/\s+(\d+)/g, '\t$1')
+      .replace(/\s+\//g, '\t/')
+      .split(/\t/)
+  ))
+}
+
 function createWindow() {
+  let payload = JSON.stringify(df())
   let win = new BrowserWindow({})
   win.maximize()
-  win.loadFile("index.html")
+  win.loadFile('index.html') // loads the html in the index.html
+
+  win.webContents.on('did-finish-load', function () {
+    win.webContents.executeJavaScript(`displayFreeDiskSpace(${payload})`)
+  })
 }
+
 
 app.on("ready", createWindow)
 
