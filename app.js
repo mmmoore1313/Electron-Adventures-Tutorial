74af343385d05a71e5470ada3345a8ccd7343ee1
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-09 13:30:07 -0500

    - Commented out episode 6

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
+// episode 7: Visualize Free Disk Space in Terminal
+
 // episode 6: Use D3 to Visualize Free Disk Space
-function displayFreeDiskSpace(data) {
-  let info = d3.select("#info")
-
-  for (let row of data) {
-    let total = parseInt(row[1])
-    let used = parseInt(row[2])
-    let free = total - used
-    let path = row[5]
-
-    let data = { free, used }
-
-    let div = info.append("div")
-    div.append("h3").text(path)
-
-    let svg = div
-      .append("svg")
-      .attr("width", "100px")
-      .attr("height", "100px")
-    let g = svg
-      .append("g")
-      .attr("transform", "translate(50,50)")
-
-    let pie = d3.pie().value(d => d[1])
-    let pieData = pie(Object.entries(data))
-    let color = d3
-      .scaleOrdinal()
-      .domain(["used", "free"])
-      .range(["red", "green"])
-
-    g
-      .selectAll("path")
-      .data(pieData)
-      .enter()
-      .append("path")
-      .attr("d", d3.arc()
-        .innerRadius(0)
-        .outerRadius(45)
-      )
-      .attr("fill", d => color(d.data[0]))
-      .attr("stroke", "black")
-      .style("stroke-width", "2px")
-      .style("opacity", 0.7)
-  }
-}
+// function displayFreeDiskSpace(data) {
+//   let info = d3.select("#info")
+// 
+//   for (let row of data) {
+//     let total = parseInt(row[1])
+//     let used = parseInt(row[2])
+//     let free = total - used
+//     let path = row[5]
+// 
+//     let data = { free, used }
+// 
+//     let div = info.append("div")
+//     div.append("h3").text(path)
+// 
+//     let svg = div
+//       .append("svg")
+//       .attr("width", "100px")
+//       .attr("height", "100px")
+//     let g = svg
+//       .append("g")
+//       .attr("transform", "translate(50,50)")
+// 
+//     let pie = d3.pie().value(d => d[1])
+//     let pieData = pie(Object.entries(data))
+//     let color = d3
+//       .scaleOrdinal()
+//       .domain(["used", "free"])
+//       .range(["red", "green"])
+// 
+//     g
+//       .selectAll("path")
+//       .data(pieData)
+//       .enter()
+//       .append("path")
+//       .attr("d", d3.arc()
+//         .innerRadius(0)
+//         .outerRadius(45)
+//       )
+//       .attr("fill", d => color(d.data[0]))
+//       .attr("stroke", "black")
+//       .style("stroke-width", "2px")
+//       .style("opacity", 0.7)
+//   }
+// }
 
 // episode 5: Display Free Disk Space
 // function displayFreeDiskSpace(data) {
--- index.js
+++ index.js
@@ -1,41 +1,43 @@
+// episode 7: Visualize Free Disk Space in Terminal
+
 // episode 6: Use D3 to Visualize Free Disk Space
-let child_process = require("child_process")
-let { app, BrowserWindow } = require("electron")
-
-let runCommand = (command) => {
-  return child_process.execSync(command).toString().trim()
-}
-
-function df() {
-  let output = runCommand("df -kP")
-  let rows = output
-    .split(/[\r\n]+/)
-    .slice(1)
-  return rows.map(row => (
-    row
-      .replace(/\s+(\d+)/g, '\t$1')
-      .replace(/\s+\//g, '\t/')
-      .split(/\t/)
-  ))
-}
-
-function createWindow() {
-  let payload = JSON.stringify(df())
-  let win = new BrowserWindow({})
-  win.maximize()
-  win.loadFile('index.html') // loads the html in the index.html
-
-  win.webContents.on('did-finish-load', function () {
-    win.webContents.executeJavaScript(`displayFreeDiskSpace(${payload})`)
-  })
-}
-
-
-app.on("ready", createWindow)
-
-app.on("window-all-closed", () => {
-  app.quit()
-})
+// let child_process = require("child_process")
+// let { app, BrowserWindow } = require("electron")
+// 
+// let runCommand = (command) => {
+//   return child_process.execSync(command).toString().trim()
+// }
+// 
+// function df() {
+//   let output = runCommand("df -kP")
+//   let rows = output
+//     .split(/[\r\n]+/)
+//     .slice(1)
+//   return rows.map(row => (
+//     row
+//       .replace(/\s+(\d+)/g, '\t$1')
+//       .replace(/\s+\//g, '\t/')
+//       .split(/\t/)
+//   ))
+// }
+// 
+// function createWindow() {
+//   let payload = JSON.stringify(df())
+//   let win = new BrowserWindow({})
+//   win.maximize()
+//   win.loadFile('index.html') // loads the html in the index.html
+// 
+//   win.webContents.on('did-finish-load', function () {
+//     win.webContents.executeJavaScript(`displayFreeDiskSpace(${payload})`)
+//   })
+// }
+// 
+// 
+// app.on("ready", createWindow)
+// 
+// app.on("window-all-closed", () => {
+//   app.quit()
+// })
 
 // episode 5: Display Free Disk Space
 // 
