403e488aa4fe61c3b0d6edd40232b2268c8c97d6
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-10 18:41:05 -0500

    - executes the child process function

--- preload.js
+++ preload.js
@@ -1,3 +1,7 @@
 // episode 10: Preload Script
 let child_process = require('child_process')
 let { contextBridge } = require('electron')
+
+let runCommand = (command) => {
+  return child_process.execSync(command).toString().trim()
+}
