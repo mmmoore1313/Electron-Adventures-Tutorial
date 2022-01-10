62da90af5f1f1daec3c1975261eda310e92e17ba
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-10 17:21:27 -0500

    - added the initial boilerplate for the app.js from the last episode

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 // app.js is the JS that runs the frontend
 // episode 10: Preload Script
+let child_process = require("child_process")
 
+let form = document.querySelector("form")
+let input = document.querySelector("input")
+let terminalHistory = document.querySelector("#history")
+
+function createInputLine(command) {
+  let inputLine = document.createElement("div")
+  inputLine.className = "input-line"
+
+  let promptSpan = document.createElement("span")
+  promptSpan.className = "prompt"
+  promptSpan.append("$")
+  let inputSpan = document.createElement("span")
+  inputSpan.className = "input"
+  inputSpan.append(command)
+
+  inputLine.append(promptSpan)
+  inputLine.append(inputSpan)
+
+  return inputLine
+}
+
+function createTerminalHistoryEntry(command, commandOutput) {
+  let inputLine = createInputLine(command)
+  let output = document.createElement("div")
+  output.className = "output"
+  output.append(commandOutput)
+  terminalHistory.append(inputLine)
+  terminalHistory.append(output)
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let command = input.value
+  let output = child_process.execSync(command).toString().trim()
+  createTerminalHistoryEntry(command, output)
+  input.value = ""
+  input.scrollIntoView()
+})
 // episode 9: Terminal App   
 // let child_process = require("child_process")
 // 
