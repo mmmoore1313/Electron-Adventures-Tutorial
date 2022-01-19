58d47a905fc8ec9202cb61cca7868f1ce2e0b22d
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-19 16:55:47 -0500

    - Wrapped the child_process in a promise wrapped in a callback

--- preload.js
+++ preload.js
@@ -2,7 +2,11 @@ let child_process = require("child_process")
 let { contextBridge } = require("electron")
 
 let runCommand = (command) => {
-  return child_process.execSync(command).toString().trim()
+  return new Promise((resolve, reject) => {
+    child_process.execSync(command, (error, stdout, stderr) => {
+      resolve({stdout, stderr, error})
+    })
+  })
 }
 
 contextBridge.exposeInMainWorld(
