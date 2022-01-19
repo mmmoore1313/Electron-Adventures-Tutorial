f3c8014933525891c205ac24297770ba8df10de0
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-18 21:02:06 -0500

    - created and filled out the index.js - specified windwo size

---
+++ index.js
@@ -0,0 +1,18 @@
+let { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 900, 
+    height: 680,
+    webPreferences: {
+      preload: `${__dirname}/preload.js`
+    },
+  })
+  win.loadURL("http://localhost:5000/")
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
