ae88d76f25f93675b93551289d788edb57949818
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-19 16:48:22 -0500

    - commented out the og svelte code

--- src/HistoryEntry.svelte
+++ src/HistoryEntry.svelte
@@ -25,3 +25,34 @@
     flex: 1;
   }
 </style>
+
+<!-- 
+episode 13: svelte
+
+<script>
+  export let command, output
+</script>
+
+<div class='input-line'>
+  <span class='prompt'>$</span>
+  <span class='input'>{command}</span>
+</div>
+<div class='output'>{output}</div>
+
+<style>
+  .output {
+    color: #afa;
+    white-space: pre;
+    padding-bottom: 0.5rem;
+  }
+  
+  .input-line {
+    display: flex;
+    gap: 0.5rem;
+  }
+  
+  .input {
+    color: #ffa;
+    flex: 1;
+  }
+</style> -->
