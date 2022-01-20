2202ef17dbac052ced294020a1a4675ccef05e0f
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-19 22:47:53 -0500

    - implemented the spinners from svelte-awesome - added the error and running flags - rejiggered the html to display the changes

--- src/HistoryEntry.svelte
+++ src/HistoryEntry.svelte
@@ -1,25 +1,42 @@
 <script>
-  export let command, output
+  import Icon from 'svelte-awesome'
+  import { spinner, exclamationTriangle } from 'svelte-awesome/icons'
+  
+  export let command, stdout, stderr, error, running
 </script>
 
-<div class='input-line'>
-  <span class='prompt'>$</span>
-  <span class='input'>{command}</span>
+<div class='history-entry'>
+  <div class='input-line'>
+    <span class='prompt'>$</span>
+    <span class='input'>{command}</span>
+  </div>
+  <div class='stdout'>{stdout}</div>
+  <div class='stderr'>{stderr}</div>
+  {#if running}
+    <Icon data={data={exclamationTriangle}} />
+  {/if}
 </div>
-<div class='output'>{output}</div>
 
 <style>
-  .output {
-    color: #afa;
-    white-space: pre;
+  .history-entry {
     padding-bottom: 0.5rem;
   }
-  
+
+  .stdout {
+    color: #afa;
+    white-space: pre;
+  }
+
+  .stderr {
+    color: #faa;
+    white-space: pre;
+  }
+
   .input-line {
     display: flex;
     gap: 0.5rem;
   }
-  
+
   .input {
     color: #ffa;
     flex: 1;
