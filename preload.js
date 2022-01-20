1086d6781352813179cc0b20c980ac3091347995
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-20 15:39:03 -0500

    - Commented out the code from the previous episode

--- preload.js
+++ preload.js
@@ -24,6 +24,34 @@ contextBridge.exposeInMainWorld(
   "api", { runCommand }
 )
 
+// episode 17: Terminal Input
+// 
+// let child_process = require("child_process")
+// let { contextBridge } = require("electron")
+// 
+// let runCommand = ({command, onout, onerr, ondone}) => {
+//   const proc = child_process.spawn(
+//     command,
+//     [],
+//     {
+//       shell: true,
+//       stdio: ["pipe", "pipe", "pipe"],
+//     },
+//   )
+//   proc.stdout.on("data", (data) => onout(data.toString()))
+//   proc.stderr.on("data", (data) => onerr(data.toString()))
+//   proc.on("close", (code) => ondone(code))
+//   return {
+//     kill: () => proc.kill(),
+//     input: (data) => proc.stdin.write(data),
+//     endInput: () => proc.stdin.end(),
+//   }
+// }
+// 
+// contextBridge.exposeInMainWorld(
+//   "api", { runCommand }
+// )
+
 // episode 16: Streaming
 // let child_process = require("child_process")
 // let { contextBridge } = require("electron")
