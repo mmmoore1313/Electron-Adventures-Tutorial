0218e2e95fbbf7c3d14fd0555e765e9fe10f9fee
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-20 02:00:14 -0500

    - commented out the last episode

--- preload.js
+++ preload.js
@@ -19,6 +19,28 @@ contextBridge.exposeInMainWorld(
   "api", { runCommand }
 )
 
+// episode 16: Streaming
+// let child_process = require("child_process")
+// let { contextBridge } = require("electron")
+// 
+// let runCommand = ({command, onout, onerr, ondone}) => {
+//   const proc = child_process.spawn(
+//     command,
+//     [],
+//     {
+//       shell: true,
+//       stdio: ["ignore", "pipe", "pipe"],
+//     },
+//   )
+//   proc.stdout.on("data", (data) => onout(data.toString()))
+//   proc.stderr.on("data", (data) => onerr(data.toString()))
+//   proc.on("close", (code) => ondone(code))
+// }
+// 
+// contextBridge.exposeInMainWorld(
+//   "api", { runCommand }
+// )
+
 // episode 15: Async
 //
 // let child_process = require("child_process")
