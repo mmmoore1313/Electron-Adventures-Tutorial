b4d2254da1e402ef096539ecc255e597fbf1d20d
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-20 15:18:18 -0500

    - moved most of the command structure out to Command.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import HistoryEntry from "./HistoryEntry.svelte"
+  import Command from "./HistoryEntry.svelte"
   import CommandInput from "./CommandInput.svelte"
   
   let history = []
@@ -8,20 +8,6 @@
     let entry = {command, stdout: "", stderr: "", error: null, running: true}
     history.push(entry)
     history = history
-    let onout = (data) => {
-      entry.stdout += data
-      history = history
-    }
-    let onerr = (data) => {
-      entry.stderr += data
-      history = history
-    }
-    let ondone = (code) => {
-      entry.running = false
-      entry.error = (code !== 0)
-      history = history
-    }
-    window.api.runCommand({command,onout,onerr,ondone}))
   }
 </script>
 
@@ -30,7 +16,7 @@
 <div id="terminal">
   <div id="history">
     {#each history as entry}
-      <HistoryEntry {...entry} />
+      <Command command={entry.command} />
     {/each}
   </div>
 
