36ac618af92dea6b42cc29c17106757fee476d7c
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-26 16:43:42 -0500

    - commented out the last episodes code to demonstrate change

--- preload.js
+++ preload.js
@@ -17,6 +17,26 @@ contextBridge.exposeInMainWorld(
   "api", { directoryContents, currentDirectory }
 )
 
+// ep21: File Manager
+// let { readdir } = require("fs/promises")
+// let { contextBridge } = require("electron")
+// 
+// let directoryContents = async (path) => {
+//   let results = await readdir(path, {withFileTypes: true})
+//   return results.map(entry => ({
+//     name: entry.name,
+//     type: entry.isDirectory() ? "directory" : "file",
+//   }))
+// }
+// 
+// let currentDirectory = () => {
+//   return process.cwd()
+// }
+// 
+// contextBridge.exposeInMainWorld(
+//   "api", { directoryContents, currentDirectory }
+// )
+
 // ep13: Svelte
 // let child_process = require("child_process")
 // let { contextBridge } = require("electron")
