8a941e827cd66433fca18adc32fe45b8a03e25b9
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-26 18:14:07 -0500

    - moved the preload script to api.js

--- preload.js
+++ preload.js
@@ -1,67 +1,7 @@
-let path = require("path")
-let { readdir, stat, readlink } = require("fs/promises")
 let { contextBridge } = require("electron")
+let api = require("./preload/api")
 
-let fileInfo = async (basePath, entry) => {
-  let {name} = entry
-  let fullPath = path.join(basePath, name)
-  let linkTarget = null
-  let fileStat
-
-
-  if (entry.isSymbolicLink()) {
-    linkTarget = await readlink(fullPath)
-  }
-
-  // this is for broken symlinks, and is a protection against deleted files
-  try {
-    fileStat = await stat(fullPath)
-  } catch {
-    return {
-      name,
-      type: "broken",
-      linkTarget,
-    }
-  }
-
-  let {size, mtime} = fileStat
-
-  if (fileStat.isDirectory()) {
-    return {
-      name,
-      type: "directory",
-      mtime,
-      linkTarget,
-    }
-  } else if (fileStat.isFile()) {
-    return {
-      name,
-      linkTarget,
-      type: "file",
-      size,
-      mtime,
-      linkTarget,
-    }
-  } else {
-    return {
-      name,
-      type: "special",
-    }
-  }
-}
-
-let directoryContents = async (path) => {
-  let results = await readdir(path, {withFileTypes: true})
-  return await Promise.all(results.map(entry => fileInfo(path, entry)))
-}
-
-let currentDirectory = () => {
-  return process.cwd()
-}
-
-contextBridge.exposeInMainWorld(
-  "api", { directoryContents, currentDirectory }
-)
+contextBridge.exposeInMainWorld("api", api)
 
 // ep23: Display Info About Files
 // let path = require("path")
