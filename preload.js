67747598a097469db1a615d85ef74d4999810e4c
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-26 17:51:03 -0500

    commented out the previous episode

--- preload.js
+++ preload.js
@@ -63,6 +63,72 @@ contextBridge.exposeInMainWorld(
   "api", { directoryContents, currentDirectory }
 )
 
+// ep23: Display Info About Files
+// let path = require("path")
+// let { readdir, stat, readlink } = require("fs/promises")
+// let { contextBridge } = require("electron")
+// 
+// let fileInfo = async (basePath, entry) => {
+//   let {name} = entry
+//   let fullPath = path.join(basePath, name)
+//   let linkTarget = null
+//   let fileStat
+// 
+// 
+//   if (entry.isSymbolicLink()) {
+//     linkTarget = await readlink(fullPath)
+//   }
+// 
+//   // this is for broken symlinks, and is a protection against deleted files
+//   try {
+//     fileStat = await stat(fullPath)
+//   } catch {
+//     return {
+//       name,
+//       type: "broken",
+//       linkTarget,
+//     }
+//   }
+// 
+//   let {size, mtime} = fileStat
+// 
+//   if (fileStat.isDirectory()) {
+//     return {
+//       name,
+//       type: "directory",
+//       mtime,
+//       linkTarget,
+//     }
+//   } else if (fileStat.isFile()) {
+//     return {
+//       name,
+//       linkTarget,
+//       type: "file",
+//       size,
+//       mtime,
+//       linkTarget,
+//     }
+//   } else {
+//     return {
+//       name,
+//       type: "special",
+//     }
+//   }
+// }
+// 
+// let directoryContents = async (path) => {
+//   let results = await readdir(path, {withFileTypes: true})
+//   return await Promise.all(results.map(entry => fileInfo(path, entry)))
+// }
+// 
+// let currentDirectory = () => {
+//   return process.cwd()
+// }
+// 
+// contextBridge.exposeInMainWorld(
+//   "api", { directoryContents, currentDirectory }
+// )
+
 // ep21: File Manager
 // let { readdir } = require("fs/promises")
 // let { contextBridge } = require("electron")
