adc27e30efdccd883a3fa18060ca7e9a7f5f67a8
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-26 14:03:58 -0500

    - commented out previous code

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ ipcRenderer.on("line", (event, line) => {
   message.textContent = line
   messages.appendChild(message)
 })
+// ep19 Data to Front End
+// let { ipcRenderer } = require('electron')
+// let messages = document.querySelector('#messages')
+// 
+// ipcRenderer.on("line", (event, line) => {
+//   let message = document.createElement("div")
+//   message.textContent = line
+//   messages.appendChild(message)
+// })
--- index.js
+++ index.js
@@ -26,3 +26,32 @@ let rl = readline.createInterface({
 rl.on("line", async (line) => {
   win.webContents.send("line", line)
 })
+// ep 19 Data 2 Front End
+// let { app, BrowserWindow } = require('electron')
+// let win
+// 
+// function createWindow() {
+//   win = new BrowserWindow({
+//     webPreferences: {
+//       nodeIntegration: true,
+//       contextIsolation: false,
+//     }
+//   })
+//   win.loadFile("index.html")
+// }
+// 
+// app.on("ready", createWindow)
+// app.on("window-all-closed", () => {
+//   app.quit()
+// })
+// 
+// let readline = require("readline")
+// 
+// let rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// })
+// 
+// rl.on("line", async (line) => {
+//   win.webContents.send("line", line)
+// })
