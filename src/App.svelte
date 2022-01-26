db1a00a9776c99eb03715f8408cf6642f22ce752
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-26 15:17:56 -0500

    - added = to fix typo

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,48 @@
 <script>
-  import HistoryEntry from "./HistoryEntry.svelte"
-  import CommandInput from "./CommandInput.svelte"
-  let history = []
-  function onsubmit(command) {
-    let output = window.api.runCommand(command)
-    history.push({command, output})
-    history = history
+  let directory = window.api.currentDirectory()
+  $: filesPromise = window.api.directoryContents(directory)
+  $: isRoot = (directory === "/")
+  
+  function navigate(path) {
+    if (directory === "/") {
+      directory = "/" + path
+    } else {
+      directory += "/" + path
+    }
+  }
+  function navigateUp() {
+    directory = directory.split("/").slice(0, -1).join("/") || "/"
   }
 </script>
 
-<h1>Svelte Terminal App</h1>
+<h1>{directory}</h1>
 
-<div id="terminal">
-  <div id="history">
-    {#each history as entry}
-      <HistoryEntry {...entry} />
-    {/each}
-  </div>
-
-  <CommandInput {onsubmit} />
-</div>
+{#await filesPromise}
+{:then files}
+  {#if !isRoot}
+    <div>
+      <button on:click={() => navigateUp()}>..</button>
+    </div>
+  {/if}
+  {#each files as entry}
+    {#if entry.type === "directory"}
+      <div>
+        <button on:click={() => navigate(entry.name)}>
+          {entry.name}
+        </button>
+      </div>
+    {:else}
+      <div>
+        {entry.name}
+      </div>
+    {/if}
+  {/each}
+{/await}
 
 <style>
 :global(body) {
   background-color: #444;
-  color: #fff;
-  font-family: monospace;
+  color: #ccc;
 }
 </style>
 
