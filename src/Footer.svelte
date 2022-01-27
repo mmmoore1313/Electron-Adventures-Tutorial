25f8e6d2805e2f7ded2e20df12f024064c225dee
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-27 18:23:22 -0500

    - added F buttons - experimented with svelte to reduce the amount of typing

---
+++ src/Footer.svelte
@@ -0,0 +1,35 @@
+<script>
+  let opts = [
+    { name: 'F1 Help' },
+    { name: 'F2 Menu' },
+    { name: 'F3 View' },
+    { name: 'F4 Edit' },
+    { name: 'F5 Copy' },
+    { name: 'F6 Move' },
+    { name: 'F7 Mkdir' },
+    { name: 'F8 Delete' },
+    { name: 'F10 Quit' },
+  ]
+</script>
+
+<footer>
+  {#each opts as { name } }
+    <button>
+      {name}
+    </button>
+  {/each}
+</footer>
+
+<style>
+  footer {
+    text-align: center;
+    grid-area: footer;
+  }
+
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    background-color: #66b;
+    color: inherit;
+  }
+</style>
