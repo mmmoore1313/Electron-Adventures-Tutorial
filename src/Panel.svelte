ff6352721dac332026c3ab2578084d039e68018b
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-27 18:49:38 -0500

    - commented out the previous episode in these files as they are the ones to experience change

--- src/Panel.svelte
+++ src/Panel.svelte
@@ -56,3 +56,64 @@
     background-color: #66b;
   }
 </style>
+
+<!-- 
+ep 26: Svelte Orthodox File Manager
+<script>
+  export let position
+  export let files
+  export let active
+  export let onActivate
+  
+  let focused = files[0]
+  let selected = []
+  let onclick = (file) => {
+    onActivate(position)
+    focused = file
+  }
+  let onrightclick = (file) => {
+    onActivate(position)
+    focused = file
+    if (selected.includes(file)) {
+      selected = selected.filter(f => f !== file)
+    } else {
+      selected = [...selected, file]
+    }
+  }
+</script>
+
+<div class="panel {position}" class:active={active}>
+  {#each files as file}
+    <div
+      class="file"
+      class:focused={file === focused}
+      class:selected={selected.includes(file)}
+      on:click|preventDefault={() => onrightclick(file)}
+    >
+      {file}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .panel-left {
+    grid-area: panel-left;
+  }
+  .panel-right {
+    grid-area: panel-right;
+  }
+  .panel {
+    background: #338;
+    margin: 4px;
+  }
+  .file {
+    cursor: pointer;
+  }
+  .file.selected {
+    color: #ff2;
+    font-weight: bold;
+  }
+  .panel.active .file.focused {
+    background-color: #66b;
+  }
+</style> -->
