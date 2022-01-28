e1899af6d33854dbdc8d8899e0c7b37c6e977647
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-27 22:48:03 -0500

    - attempting to use v-for function to map over the button for each of the F-keys - added styling

---
+++ src/Footer.vue
@@ -0,0 +1,39 @@
+<script>
+  items: [
+    { name: 'F1 Help' },
+    { name: 'F2 Menu' },
+    { name: 'F3 View' },
+    { name: 'F4 Edit' },
+    { name: 'F5 Copy' },
+    { name: 'F6 Move' },
+    { name: 'F7 Mkdir' },
+    { name: 'F8 Delete' },
+    { name: 'F10 Quit' },
+  ]
+  
+  export default {
+    name: "Footer"
+  }
+</script>
+
+<template>
+  <footer>
+    <button v-for="item in items" :key="item.name">
+      {{ item.name }}
+    </button>
+  </footer>
+</template>
+
+<style>
+  footer {
+    text-align: center;
+    grid-area: footer;
+  }
+
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    background-color: #66b;
+    color: inherit;
+  }
+</style>
