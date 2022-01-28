1586305fbe59a92538f069874b69ffb4c91c41c1
Matt Moore <Matt.M.Moore13@outlook.com>
2022-01-27 22:35:25 -0500

    - commented out the previous code

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,22 @@ export default {
     colo: #fff;
   }
 </style>
+
+<!-- 
+ep 28: Vue
+<template>
+  <h1>Welcome to Vue + Electron!</h1>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style global>
+  body{
+    backlground-color: #444;
+    colo: #fff;
+  }
+</style> -->
