74f6ee2d180702bbf25a413ce7476291e2645cd3
Matt Moore <Matt.M.Moore13@outlook.com>
2022-02-02 18:21:02 -0500

    getting it up and running

--- src/App.svelte
+++ src/App.svelte
@@ -40,12 +40,29 @@
 
 <style>
   :global(body) {
-    background-color: #444;
-    color: #ccc;
+    background-color: #226;
+    color: #fff;
+    font-family: monospace;
+    margin: 0;
+    font-size: 16px;
   }
-  .file-list {
+  .ui {
+    width: 100vw;
+    height: 100vh;
     display: grid;
-    grid-template-columns: 3fr 1fr 1fr 1fr;
+    grid-template-areas:
+      "header header"
+      "panel-left panel-right"
+      "footer footer";
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: auto minmax(0, 1fr) auto;
+  }
+  .ui header {
+    grid-area: header;
+  }
+  header {
+    font-size: 24px;
+    margin: 4px;
   }
 </style>
 
--- src/Panel.svelte
+++ src/Panel.svelte
@@ -1,5 +1,118 @@
 <script>
   export let position
+  export let directory
+  export let active
+  export let onActivate
+  
+  $: filesPromise = window.api.directoryContents(directory)
+  $: filesPromise.then(x => {
+    files = x
+    focusedIdx = 0
+    selected = []
+  })
+  $: filesCount = files.length
+
+  let onclick = (idx) => {
+    onActivate()
+    focusedIdx = idx
+  }
+  let onrightclick = (idx) => {
+    onActivate()
+    focusedIdx = idx
+    flipSelected(idx)
+  }
+  let flipSelected = (idx) => {
+    if (selected.includes(idx)) {
+      selected = selected.filter(f => f !== idx)
+    } else {
+      selected = [...selected, idx]
+    }
+  }
+  let goUp = () => {
+    if (focusedIdx > 0) {
+      focusedIdx -= 1
+    }
+  }
+  let goDown = () => {
+    if (focusedIdx < filesCount -1) {
+      focusedIdx += 1
+    }
+  }
+  let handleKey = (e) => {
+    if (!active) {
+      return
+    }
+    if (e.key == "ArrowDown") {
+      e.preventDefault()
+      goDown()
+    }
+    if (e.key === "ArrowUp") {
+      e.preventDefault()
+      goUp()
+    }
+    if (e.key === " ") {
+      e.preventDefault()
+      flipSelected(focusedIdx)
+      goDown()
+    }
+  }
+</script>
+
+<div class="panel {position}" class:active={active}>
+  <header>
+    {directory.split("/").slice(-1)[0]}
+  </header>
+  <div class="file-list">
+    {#each files as file, idx}
+      <div
+        class="file"
+        class:focused={idx === focusedIdx}
+        class:selected={selected.includes(idx)}
+        on:click|preventDefault={() => onclick(idx)}
+        on:contextmenu|preventDefault={() => onrightclick(idx)}
+      >
+        {file.name}
+      </div>
+    {/each}
+  </div>
+</div>
+
+<svelte:window on:keydown={handleKey}/>
+
+<style>
+  .left {
+    grid-area: panel-left;
+  }
+  .right {
+    grid-area: panel-right;
+  }
+  .panel {
+    background: #338;
+    margin: 4px;
+    display: flex;
+    flex-direction: column;
+  }
+  header {
+    text-align: center;
+    font-weight: bold;
+  }
+  .file-list {
+    flex: 1;
+    overflow-y: scroll;
+  }
+  .file.selected {
+    color: #ff2;
+    font-weight: bold;
+  }
+  .panel.active .file.focused {
+    background-color: #66b;
+  }
+</style>
+
+<!-- 
+ep27: keyboard stuffs
+<script>
+  export let position
   export let files
   export let active
   export let onActivate
@@ -90,7 +203,7 @@
   .panel.active .file.focused {
     background-color: #66b;
   }
-</style>
+</style> -->
 
 <!-- 
 ep 26: Svelte Orthodox File Manager
